00d3d221493bb78be20c784424084d89d5c4af83
Sustain Acc <ccarlson355@gmail.com>
2021-04-18 16:26:59 -0600

    process all files

--- process.py
+++ process.py
@@ -1,4 +1,5 @@
 import pygrib
+import time
 import pymongo
 import sys
 import os
@@ -45,14 +46,12 @@ def euclidean_distance(lat1, lon1, lat2, lon2):
     return math.sqrt( (lat1 - lat2)**2 + (lon1 - lon2)**2 )
 
 
-
 def find_gisjoin_indices(file_path):
     grbs = pygrib.open(file_path)
     grb  = grbs.message(1)
     lats, lons = grb.latlons()
     latsize = lats.shape[0]
     lonsize = lons.shape[1]
-
 
     for index, center in enumerate(gisjoin_centers):
         min_distance = sys.maxsize # infinity
@@ -78,135 +77,145 @@ def find_gisjoin_indices(file_path):
 
 
 
-def convert(year_month, year_month_day, filename):
-
+def convert_grb_to_csv(file_path, out_path, year, month, day, hour, timestep):
     grbs = pygrib.open(file_path)
     grb = grbs.message(1)
     lats, lons = grb.latlons()
     latsize = lats.shape[0]
     lonsize = lons.shape[1]
 
-    top_left_lat = 49.0
-    top_left_lon = -125.0
-    bottom_right_lat = 25.0
-    bottom_right_lon = -67.0
-
-    min_lat_index = 99999
-    max_lat_index = -1
-    min_lon_index = 99999
-    max_lon_index = -1
-
-    for lat_index in range(latsize):
-        for lon_index in range(lonsize):
-            lat = lats[lat_index][lon_index]
-            lon = lons[lat_index][lon_index]
-
-            if lat < top_left_lat and lat > bottom_right_lat and lon > top_left_lon and lon < bottom_right_lon:
-                if lat_index < min_lat_index:
-                    min_lat_index = lat_index
-                if lat_index > max_lat_index:
-                    max_lat_index = lat_index
-                if lon_index < min_lon_index:
-                    min_lon_index = lon_index
-                if lon_index > max_lon_index:
-                    max_lon_index = lon_index
-
-    print(f"Min lat,lon indices ({min_lat_index},{min_lon_index}) at lat,lon ({lats[min_lat_index][min_lon_index]},{lons[min_lat_index][min_lon_index]})")
-    print(f"Max lat,lon indices ({max_lat_index},{max_lon_index}) at lat,lon ({lats[max_lat_index][max_lon_index]},{lons[max_lat_index][max_lon_index]})")
-    lat_index_count = max_lat_index - min_lat_index
-    lon_index_count = max_lon_index - min_lon_index
-    total_index_count = lat_index_count * lon_index_count
-    print(f"Creating a box of [{lat_index_count},{lon_index_count}], or {total_index_count} indices")
-
-
-    #data, lats, lons = grb.data(lat1=48,lat2=25,lon1=-125,lon2=-66)
-    #print(f"Data shape: {data.shape}")
-    print(f"Latitude shape:  {lats.shape}")
-    print(f"Longitude shape: {lons.shape}")
-
-    # total   = latsize * lonsize
-
     selected_fields = [
-                (2,  "Mean sea level pressure", "mean_sea_level_pressure_pascal"),
-                (3,  "Surface pressure", "surface_pressure_surface_level_pascal"),
-                (4,  "Orography", "orography_surface_level_meters"),
-                (5,  "Temperature", "temp_surface_level_kelvin"),
-                (6,  "2 metre temperature", "2_metre_temp_kelvin"),
-                (7,  "2 metre dewpoint temperature", "2_metre_dewpoint_temp_kelvin"),
-                (8,  "Relative humidity", "relative_humidity_percent"),
-                (9,  "10 metre U wind component", "10_metre_u_wind_component_meters_per_second"),
-                (10, "10 metre V wind component", "10_metre_v_wind_component_meters_per_second"),
-                (11, "Total Precipitation", "total_precipitation_kg_per_squared_meter"),
-                (12, "Convective precipitation (water)", "water_convection_precipitation_kg_per_squared_meter"),
-                (13, "Convective precipitation (water)", "water_convection_precipitation_kg_per_squared_meter"),
-            ]
+            (2,   "Mean sea level pressure", "mean_sea_level_pressure_pascal"),
+            (3,   "Surface pressure", "surface_pressure_surface_level_pascal"),
+            (4,   "Orography", "orography_surface_level_meters"),
+            (5,   "Temperature", "temp_surface_level_kelvin"),
+            (6,   "2 metre temperature", "2_metre_temp_kelvin"),
+            (7,   "2 metre dewpoint temperature", "2_metre_dewpoint_temp_kelvin"),
+            (8,   "Relative humidity", "relative_humidity_percent"),
+            (9,   "10 metre U wind component", "10_metre_u_wind_component_meters_per_second"),
+            (10,  "10 metre V wind component", "10_metre_v_wind_component_meters_per_second"),
+            (11,  "Total Precipitation", "total_precipitation_kg_per_squared_meter"),
+            (12,  "Convective precipitation (water)", "water_convection_precipitation_kg_per_squared_meter"),
+            (291, "Soil Temperature", "soil_temperature_kelvin"),
+            (339, "Pressure", "pressure_pascal"),
+            (347, "Visibility", "visibility_meters"),
+            (348, "Precipitable water", "precipitable_water_kg_per_squared_meter"),
+            (358, "Total Cloud Cover", "total_cloud_cover_percent"),
+            (362, "Snow depth", "snow_depth_meters"),
+            (382, "Ice cover (1=ice, 0=no ice)", "ice_cover_binary")
+        ]
 
-    '''
+    out_file = f"{out_path}/{year}_{month}_{day}_{hour}_{timestep}.csv"
+    print(f"Processing/Writing {out_file}...")
 
-    # Each grb value is of shape (428, 614), or roughly 262,792 entries
     with open(out_file, "w") as f:
-
         # Create csv header row with new column names
-        csv_header_row = "year_month_day_hour,"
+        csv_header_row = "year_month_day_hour,timestep,gis_join"
         csv_header_row += ",".join([field[2] for field in selected_fields])
         f.write(csv_header_row + "\n")
 
-        # Iterate over all lat/lon indices
-        current = 1
-        for x in range(latsize):
-            for y in range(lonsize):
-                
-                # Create csv row with first element being timestamp
-                csv_row = f"{year_month_day_hour},"
+        year_month_day_hour = f"{year}{month}{day}{hour}"
 
-                # Iterate over all selected variables
-                for selected_field in selected_fields:
-                    index = selected_field[0]
-                    actual_field_name = selected_field[1]
+        for index, center in enumerate(gisjoin_centers):
+            csv_row = f"{year_month_day_hour},{timestep},{center.gisjoin}"
 
-                    # Get next variable's value for lat/lon pair and add to csv row
-                    grbs.seek(index-1)
-                    grb = grbs.read(1)[0]
-                    value_for_field = grb.values[x][y]
+            for selected_field in selected_fields:
+                    grb = grbs.message(selected_field[0])
+                    value_for_field = grb.values[center.lat_index][center.lon_index]
                     csv_row += f",{value_for_field}"
-                
 
-                f.write(csv_row + "\n")
-                print("Lat: %.7f\tLon: %.7f\t[%d / %d]" % (lats[x][y], lons[x][y], current, total))
-                current += 1
-                grbs.seek(0) # Go back to beginning
+            f.write(csv_row + "\n")
+            
+            # print(f"  [{index+1}/{len(gisjoin_centers)} Wrote csv row: {csv_row}")
 
-    '''
     grbs.close()
 
 
 def print_usage():
-    print("\nUSAGE:\n\tpython3 process.py <year_month> <year_month_day> <grb_filename>")
-    print("\nEXAMPLE:\n\tpython3 process.py 201001 20100105 namanl_218_20100105_0000_000.grb\n")
+    print("\nUSAGE:\n\tpython3 process.py <year>")
+    print("\nEXAMPLE:\n\tpython3 process.py 2010\n")
+
+
+def test():
+    for month in range (1, 13):
+        print(str(month).zfill(2))
+       
+def count_files(prefix, year):
+    count = 0
+    for month in range(1, 13):
+        month_str = str(month).zfill(2)
+        year_month_dir_path = f"{prefix}/{year}{month_str}"
+
+        for day_dir in sorted(os.listdir(year_month_dir_path)):
+            day_dir_path = f"{year_month_dir_path}/{day_dir}"
+            filenames = os.listdir(day_dir_path)
+            grb_filenames = [ filename for filename in filenames if filename.endswith(".grb") ]
+            count += len(grb_filenames)
+
+    return count
+
+
+def time_elapsed(t1, t2):
+    diff = t2 - t1
+    seconds = diff
+    minutes = -1
+    hours = -1
+
+    retval = f"{sec} seconds"
+    if seconds > 60:
+        minutes = seconds // 60
+        seconds = seconds % 60
+        retval = f"{minutes} minutes, {seconds} seconds"
+    if minutes > 60:
+        hours = minutes // 60
+        minutes = minutes % 60
+        retval = f"{hours} hours, {minutes} minutes, {seconds} seconds"
+
+    return retval
+
 
 
 def main():
-    if len(sys.argv) < 4:
+    if len(sys.argv) < 2:
        print_usage()
        exit(1)
 
+    data_dirs_prefix = "/s/lattice-100/a/nobackup/galileo/sustain/noaa-nam/www.ncei.noaa.gov/data/north-american-mesoscale-model/access/historical/analysis"
+    out_file_prefix = "/s/lattice-100/d/nobackup/galileo/NOAA/processed_csv"
+    year_str = sys.argv[1]
 
-    year_month = sys.argv[1]
-    year_month_day = sys.argv[2]
-    filename = sys.argv[3]
-
-    data_prefix = "/s/lattice-100/a/nobackup/galileo/sustain/noaa-nam/www.ncei.noaa.gov/data/north-american-mesoscale-model/access/historical/analysis"
-    year_month_day_hour = "201001050000"
-    year_month_day_hour_int = int(year_month_day_hour)
-    file_path = f"{data_prefix}/{year_month}/{year_month_day}/{filename}"
-
-    out_path = "/s/lattice-100/d/nobackup/galileo/NOAA/processed_csv"
-    out_filename = filename[:-4]
-    out_file = f"{out_path}/{out_filename}.csv"
-
-    # convert(sys.argv[1], sys.argv[2], sys.argv[3])
     init_gisjoin_centers()
-    find_gisjoin_indices(file_path)
+    gisjoin_indices_found = False
+    start_time = time.time()
+    file_count = 0
+    total_files = count_files(data_dirs_prefix, year_str)
+
+    for month in range (1, 13):
+        month_str = str(month).zfill(2)
+        year_month_dir_path = f"{data_dirs_prefix}/{year_str}{month_str}"
+
+        for day_dir in sorted(os.listdir(year_month_dir_path)):
+            day_str = day_dir[len(day_dir)-2:]
+            day_dir_path = f"{year_month_dir_path}/{day_dir}"
+            filenames = os.listdir(day_dir_path)
+            grb_filenames = [ filename for filename in filenames if filename.endswith(".grb") ]
+
+            for grb_file in sorted(grb_filenames):
+                hour_str = grb_file[20:22]
+                timestep = grb_file[27:28]
+                grb_file_path = f"{day_dir_path}/{grb_file}"
+                
+                if not gisjoin_indices_found:
+                    find_gisjoin_indices(grb_file_path)
+                    gisjoin_indices_found = True
+
+                convert_grb_to_csv(grb_file_path, out_file_prefix, year_str, month_str, day_str, hour_str, timestep)
+                file_count += 1
+                print(f"Finished converting file: [{file_count}/{total_files}], Time elapsed: {time_elapsed(start_time, time.time())}")
+                
+                
+
+
 
 
 if __name__ == '__main__':
