8e732a8f4818f650c6ceceeef366a6a958688987
Sustain Acc <ccarlson355@gmail.com>
2021-04-18 12:17:13 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo -e "Execute the following:\n"
-echo -e "\tpython3 -m venv . && chmod +x bin/activate"
+echo -e "\tpython3 -m venv . && chmod +x ./bin/activate"
 echo -e "\t./bin/activate"
-echo -e "\t./bin/pip3 install"
+echo -e "\t./bin/pip3 install --upgrade pip"
+echo -e "\t./bin/pip3 install cython pygrib"
 
