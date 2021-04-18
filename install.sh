83b505d0e5039ffd0c1e749f11a5893214af61ab
Sustain Acc <ccarlson355@gmail.com>
2021-04-18 12:11:30 -0600

    Gitignore most cruft

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo -e "Execute the following:\n"
-echo -e "\tpython3 -m venv ."
+echo -e "\tpython3 -m venv . && chmod +x bin/activate"
 echo -e "\t./bin/activate"
 echo -e "\t./bin/pip3 install"
 
