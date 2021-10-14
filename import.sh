2532b59a9167ec3df31598c5dc46e1b80ad60ecf
Sustain Services Repoistory <sustain@lattice-151.cs.colostate.edu>
2021-10-14 16:40:09 -0600

    Echo loggin

--- import.sh
+++ import.sh
@@ -2,5 +2,6 @@
 
 cd processed/
 for FILE in `ls $1*.csv`; do
+  echo -e "\nProcessing file $FILE...\n"
   mongoimport --host=lattice-150 --port=27018 --db=sustaindb --collection=noaa_nam_sharded --type=csv --headerline --file=$FILE
 done
