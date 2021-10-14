1d8295a3665d8730350dec62553dc38aad629980
Sustain Services Repoistory <sustain@lattice-155.cs.colostate.edu>
2021-10-14 13:17:40 -0600

    Update install script

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-echo -e "Execute the following:\n"
-echo -e "\tpython3 -m venv . && chmod +x ./bin/activate"
-echo -e "\t./bin/pip3 install --upgrade pip"
-echo -e "\t./bin/pip3 install pymongo cython pygrib"
+echo -e "Executing the following:\n"
+echo -e "\tpython3 -m venv . && chmod +x ./bin/activate && ./bin/activate;"
+echo -e "\t./bin/pip3 install --upgrade pip;"
+echo -e "\t./bin/pip3 install pymongo cython pygrib;\n"
 
+python3 -m venv . && chmod +x ./bin/activate && ./bin/activate;
+./bin/pip3 install --upgrade pip;
+./bin/pip3 install pymongo cython pygrib;
