e7e3d16bbdea5f2e5e6293f6015114a572a43256
Sustain Services Repoistory <sustain@lattice-151.cs.colostate.edu>
2021-10-17 11:05:02 -0600

    Fix usage msg for import script

--- import.sh
+++ import.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd processed/
+echo -e "Usage: ./import.sh <path_to_csv_files>"
 for FILE in `ls $1*.csv`; do
   echo -e "\nProcessing file $FILE...\n"
   mongoimport --host=lattice-150 --port=27018 --db=sustaindb --collection=noaa_nam_sharded --type=csv --headerline --file=$FILE
