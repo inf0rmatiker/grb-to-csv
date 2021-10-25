cb61ddf9c371a69694b4d0c766e658629436cb7a
Sustain Account <sustain@cs.colostate.edu>
2021-10-25 17:08:03 -0600

    Move download/import scripts to appropriate PM directory

--- import.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo -e "Usage: ./import.sh <path_to_csv_files>"
-for FILE in `ls $1*.csv`; do
-  echo -e "\nProcessing file $FILE...\n"
-  mongoimport --host=lattice-150 --port=27018 --db=sustaindb --collection=noaa_nam_sharded --type=csv --headerline --file=$FILE
-done
